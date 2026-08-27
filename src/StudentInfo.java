0f903f8c626263dc872522bdaeaca5196ae22742
keara <kearanathan14@gmail.com>
2026-08-27 10:21:58 -0400

    4 Student Attributes Added

---
+++ src/StudentInfo.java
@@ -0,0 +1,16 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class StudentInfo {
+    // Every java project will have only a and single main method
+    // Method = groping of relevant code +
+    // Return_Type (void) METHOD_NAME (main) (PARAM_Type PARM_Name){
+    //       RELEVANT CODES
+    // }
+
+    public static void main(String[] args) {
+
+        System.out.println("hello java");
+
+    }
+}
